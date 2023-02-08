1e7e98d7f6a19848b8529771543e5bf332bdbfd3
Javier Cepeda <erjavierc@gmail.com>
2023-02-08 11:37:41 -0500

    appModule, env, nestjs modules

--- backend/app-core/src/app.module.ts
+++ backend/app-core/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: parseInt(process.env.RATE_LIMIT_TTL),
+      limit: parseInt(process.env.RATE_LIMIT),
+    }),
+    MongooseModule.forRoot(
+      process.env.DB_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    )
+  ],
   providers: [AppService],
 })
 export class AppModule {}
