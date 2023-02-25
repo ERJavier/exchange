dba914ff6fc8d534df7b0c1c6853c8c27dd57739
Javier Cepeda <erjavierc@gmail.com>
2023-02-24 21:27:55 -0500

    creating user registration

--- backend/app-core/src/user/user.service.ts
+++ backend/app-core/src/user/user.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDocument, User } from '../../dist/user/schemas/user.schema';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ){}
+
+  async getUserByEmail(email: string) {
+    return this.userModel.findOne( { email } ).exec();
+  }
+
+  async register(createUserDto: CreateUserDto) {
+    const createUser = new this.userModel(createUserDto);
+    const user = await this.getUserByEmail(createUserDto.email);
   }
 }
