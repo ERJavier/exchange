a62bd22d432335330135334b6d2711196d29089b
Javier Cepeda <erjavierc@gmail.com>
2023-02-25 09:07:12 -0500

    generating blockchain wallets

--- backend/tasks/wallet-generator/evm/generate.js
+++ backend/tasks/wallet-generator/evm/generate.js
@@ -5,3 +5,33 @@ const { sleep } = require( `${appRoot}/config/utils/lock` )
 const connectDB = require(`${appRoot}/config/db/getMongoose`)
 const WalletContract = require(`${appRoot}/config/models/WalleetContract`)
 const GeneratorFactory = require('./factories/generatorFactory')
+
+var amount = process.argv[2] || 0
+const network = process.argv[3] || 97
+
+const { rpc } = require(`${appRoot}/config/chains/${network}`)
+
+connectDB.then( async () => {
+    console.log('loading contract...')
+    const generatorFactory = new GeneratorFactory(rpc)
+
+    while(amount > 0){
+        console.log('Generating wallet...', 'Remanin: ', amount - 1)
+        const res = await generatorFactory.generate()
+        const result = JSON.parse(
+            JSON.stringify(res.logs[0].args).replace('Result', '').trim()
+        )
+        
+        console.log(`Saving wallet; ${result.wallet}`)
+        const walletContract = new WalletContract({
+            address: result.wallet,
+            chainId: network
+        })
+        await walletContract.save()
+        amount--
+        await sleep(3000)
+    }
+
+    process.exit()
+
+})
